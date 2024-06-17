78d8cb5c2144396d11346178ba2362218ea1695d
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-06-17 14:08:07 +0200

    Initial commit

---
+++ src/test/java/com/example/ems_app/EmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ems_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
